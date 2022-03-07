6ab08fa424e98d0d01f478de1919d2f2038dba3b
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-07 17:48:03 -0600

    models done

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 
   # OWNER
   has_many :owned_studios, foreign_key: :owner_id, class_name: "Studio" # OwnedStudios
+  # OWNER
   has_many :owner_bookings, through: :owned_studios, source: :bookings
 
   # CUSTOMER
   has_many :customer_bookings, foreign_key: :customer_id, class_name: "Booking"
-
-  # OWNER
+  # CUSTOMER
   has_many :booked_studios, through: :customer_bookings, source: :studio
 end
