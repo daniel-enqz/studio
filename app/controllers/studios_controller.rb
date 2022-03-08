29e1083df21fc82f87d8b2ee1ba2d8b7925decf9
Edson Quintero <edson_123@me.com>
2022-03-08 16:57:33 -0600

    cards ready

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,14 +2,14 @@ class StudiosController < ApplicationController
   def index
     @studios = Studio.all
   end
-  
+
   def new
     @studio = Studio.new
   end
 
   def create
     @studio = Studio.new(studio_params)
-
+    @studio.owner = current_user
     if @studio.save
       redirect_to root, notice: 'Studio was successfully created.'
     else
