71d2889dc48ebb4d3b2c1151eeddbf146dfb21f4
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-10 16:40:37 -0600

    validations test

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :customer, class_name: "User"
   belongs_to :studio
   has_one :owner, through: :studio
+  validates :start_on, :end_on, uniqueness: true
 end
